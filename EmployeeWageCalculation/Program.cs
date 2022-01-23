de616f19309dc95c33c34708e010a85bd5c90e88
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-01-24 00:02:19 +0530

    [Arjun] Add ; Added Switched

---
+++ EmployeeWageCalculation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int FullTimeEmployee = 1;
+            int randomInput = random.Next(0, 2);
+             
+
+            if (randomInput == FullTimeEmployee)
+            {
+                Console.WriteLine("Employee is Presemt");
+                
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+              
+            }
+            Console.ReadLine();
+        }
+    }
+}

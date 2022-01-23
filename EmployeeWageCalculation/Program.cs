3b84559bf3610115a7b0ef00d57c38c1b15b857d
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-01-24 00:09:19 +0530

    [Arjun] Add ; Added Switched

--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -13,18 +13,26 @@ namespace EmployeeWageCalculation
             Random random = new Random();
             int FullTimeEmployee = 1;
             int randomInput = random.Next(0, 2);
-             
+            int empRatePerHr = 20;
+            int empHrs = 0;
+            int totalWage = 0;
+
 
             if (randomInput == FullTimeEmployee)
             {
                 Console.WriteLine("Employee is Presemt");
+                empHrs = 8;
                 
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
               
             }
+            totalWage = empRatePerHr * empHrs;
+            Console.WriteLine("Daily Wage is: " + totalWage);
+            Console.ReadLine();
             Console.ReadLine();
         }
     }
